c94d3312b6d43573116e4acb833a156b32ac266e
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-02 10:55:41 -0700

    lesson-7

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 28,
-        x: 0,
-        y: 0,
+
     },
     methods: { //You can use this to inject functions to your variables.
-        add: function (inc) {
-            this.age += inc;
-        },
-        subtract: function (dec) {
-            this.age -= dec;
+        logName: function () {
+            console.log("You entered your name");
         },
 
-        updateXY: function (event) {
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-
-        click: function () {
-            alert('You clicked me!');
+        logAge: function () {
+            console.log("You entered your age");
         }
-
     }
 }); //Don't forget the comma's inbetween each function.
