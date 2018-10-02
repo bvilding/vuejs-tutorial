83a9cc158da1a1125894c77df3928b6a80c96478
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-02 10:21:00 -0700

    Lesson-4

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Vinni',
-        job: 'Developer'
+        job: 'Developer',
+        website: 'https://www.qontact.app',
+        websiteTag: '<a href="https://www.qontact.app">Qontact.app Website</a>'
     },
     methods: {
         greet: function (time) {
