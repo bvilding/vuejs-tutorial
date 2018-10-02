ae38a91212353ba57c9f83f29222e7b65ec15ce2
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-02 10:40:16 -0700

    Lesson 5

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Vinni',
-        job: 'Developer',
-        website: 'https://www.qontact.app',
-        websiteTag: '<a href="https://www.qontact.app">Qontact.app Website</a>'
+        age: 28,
+        x: 0,
+        y: 0,
     },
-    methods: {
-        greet: function (time) {
-            return 'Good ' + time + ' ' + this.name;
+    methods: { //You can use this to inject functions to your variables.
+        add: function (inc) {
+            this.age += inc;
+        },
+        subtract: function (dec) {
+            this.age -= dec;
+        },
+
+        updateXY: function (event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
-});
+}); //Don't forget the comma's inbetween each function.
