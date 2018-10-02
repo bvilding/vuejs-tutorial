0504e3e4f79cd2f04431d2b84f88f1e01d678dbd
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-02 13:19:52 -0700

    lesson-14

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-new Vue({
-    el: '#vue-app',
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
-        health: 100,
-        ended: false,
+        title: 'Vue App One'
     },
 
     methods: { //You can use this to inject functions to your variables.
-        punch: function () {
-            this.health -= 10;
-            if (this.health <= 0) {
-                this.ended = true;
-            }
-        },
-        restart: function () {
-            this.health = 100;
-            this.ended = false;
-        },
+
     },
 
     computed: {
-
+        greet: function () {
+            return 'Hello from App One';
+        }
     },
 }); //Don't forget the comma's inbetween each function.
+
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        title: 'Vue App Two'
+
+    },
+
+    methods: { //You can use this to inject functions to your variables.
+        changeTitle: function () {
+            one.title = "Title Changed";
+        }
+    },
+
+    computed: {
+        greet: function () {
+            return 'App Two here!';
+        }
+    },
+}); //Don't forget the comma's inbetween each function.
+
+two.title = "Changed from Outside";
