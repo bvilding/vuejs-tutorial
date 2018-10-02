1ffea59d7e91c665d97c5001f62a0687371f3c81
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-02 11:19:21 -0700

    lesson-9

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: '',
-        age: '',
+        age: 20,
+        a: 0,
+        b: 0
     },
-    methods: { //You can use this to inject functions to your variables.
-        logName: function () {
 
+    methods: { //You can use this to inject functions to your variables.
+        // addToA: function () {
+        //     return this.a + this.age;
+        //         console.log('addToA');
+        // },
+
+        // addToB: function () {
+        //     return this.b + this.age;
+        //         console.log('addToA');
+        // }
+    },
+
+    computed: {
+        addToA: function () {
+            console.log('addToA');
+            return this.a + this.age;
         },
 
-        logAge: function () {
-
+        addToB: function () {
+            console.log('addToB');
+            return this.b + this.age;
         }
-    }
+    },
 }); //Don't forget the comma's inbetween each function.
