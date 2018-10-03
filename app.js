581874a6658f93ee66867680ee62a8548c3655ba
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-03 09:04:16 -0700

    lesson-16

--- app.js
+++ app.js
@@ -1,35 +1,14 @@
-Vue.component('greeting', {
-    template: '<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change Name</button></p>',
-    data: function () {
-        return {
-            name: "Vinni"
-        };
-    },
-    methods: {
-        changeName: function () {
-            this.name = "Mario";
-        }
-    }
-});
-
 new Vue({
     el: '#vue-app-one',
-
-
-    methods: { //You can use this to inject functions to your variables.
-
+    data: {
+        output: 'Your Favorite Food'
     },
 
-    computed: {
-
-    },
-}); //Don't forget the comma's inbetween each function.
-
-new Vue({
-    el: '#vue-app-two',
-
     methods: { //You can use this to inject functions to your variables.
-
+        readRefs: function () {
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
+        }
     },
 
     computed: {
