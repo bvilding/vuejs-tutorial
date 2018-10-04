bc57ec75053c5b04d2b2166949a829f11e9117bb
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-04 11:13:23 -0700

    Lesson-22

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -11,18 +11,16 @@
 
 <script>
 export default {
+  props: {
+    ninjas: {
+      type: Array,
+      required: true
+    }
+  },
+
   data() {
     // This is the function for the component. This is an ES6 function.
-    return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ]
-    };
+    return {};
   }
 };
 </script>
