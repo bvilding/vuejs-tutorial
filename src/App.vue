90cc6686dfa1f56cc256e9b1cdc00cfb2fdd99dc
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-05 14:40:28 -0700

    lesson-27

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,35 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle='updateTitle($event)'></app-header>
-    <ninjas v-bind:ninjas="ninjas"></ninjas>
-    <app-footer v-bind:title="title"></app-footer>
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>This is some information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
-import Ninjas from "./components/Ninjas.vue";
+import formHelper from "./components/formHelper";
 
 export default {
   components: {
-    "app-header": Header, //Header is already a tag and cannot be used for a component name.
-    "app-footer": Footer, //Footer is already a tag and cannot be used for a component name.
-    ninjas: Ninjas //Don't forget commas
+    "form-helper": formHelper
   },
   data() {
     // This is the function for the component. This is an ES6 function.
     return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ],
-      title: "Vue Ninjas"
+      title: "I am a dynamic Slot title" // This is where the information must reside in order for the component to work.
     };
   },
-  methods: {
-    updateTitle: function(updatedTitle) {
-      this.title = updatedTitle;
-    }
-  }
+  methods: {}
 };
 </script>
 
